0a11b51a784f580565c70fb0571cf3b6dc3a40b0
Jesus Calvo <jesus.calvo@gmail.com>
2017-12-14 16:15:37 +0000

    New gradle build

---
+++ src/main/java/com/jesuscl/testjavaec/TestJavaEcApplication.java
@@ -0,0 +1,12 @@
+package com.jesuscl.testjavaec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestJavaEcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestJavaEcApplication.class, args);
+	}
+}
---
+++ src/test/java/com/jesuscl/testjavaec/TestJavaEcApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jesuscl.testjavaec;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestJavaEcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
